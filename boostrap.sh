ef97700647e2f23d3f892595e8aad4b83e784dc7
Vicent Selfa <vtselfa@gmail.com>
2017-04-03 00:57:45 +0200

    [T] Improve boostrap.sh

--- boostrap.sh
+++ boostrap.sh
@@ -1,6 +1,9 @@
-git clone git@github.com:louisdx/cxx-prettyprint.git
-git clone git@github.com:fmtlib/fmt.git
-cd fmt
-cmake .
-make
+git clone git@github.com:louisdx/cxx-prettyprint.git &&
+git clone git@github.com:fmtlib/fmt.git &&
+cd fmt && 
+cmake . &&
+make &&
 cd ..
+git submodule init &&
+git submodule update &&
+echo "You should be able to do a 'make' now to build the project..."
